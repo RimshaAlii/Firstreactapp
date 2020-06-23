46694fff17fbf9cb26dc2387fa270f137ada853d
RimshaAli <rimsha.bsse3429@iiu.edu.pk>
2020-06-23 14:25:52 +0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ <div>Hello World.This is first react app</div>,
+  
   document.getElementById('root')
 );
 
